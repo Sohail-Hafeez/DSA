c152cdb2d09c91d8bcdc10cbfc90e5cbf5eead26
Muhammad Sohail Hafeez <sohailhafeez6464@gmail.com>
2025-09-01 14:47:55 +0500

    Time: 103 ms (54.38%), Space: 15.8 MB (47.27%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,22 +1,25 @@
 class Solution(object):
     def maxVowels(self, s, k):
-        count = 0 # initializing the count variable to keep the track
-        maxCount = 0
-        vowel = ['a','e','i','o','u'] # a list of vowel to keep checking each element of the string s
-        
-        # Firstly taking the first window or we can say first subset 
-        for i in range(0, k):
-            if s[i] in vowel:
-                count = count + 1
-        maxCount = max(count , maxCount)
-        
-        # Now taking the remaining of the string and updating count accordingly 
-        for i in range(k,len(s)):
-            if s[i] in vowel:
-                count = count + 1
+        vowels = set("aeiou")
+        count = 0
 
-            if s[i-k] in vowel:
-                count = count - 1
-            maxCount = max(count , maxCount)
+        # Count vowels in the first window
+        for i in range(k):
+            if s[i] in vowels:
+                count += 1
+
+        maxCount = count
+
+        # Slide the window
+        for i in range(k, len(s)):
+            if s[i] in vowels:   # add new char
+                count += 1
+            if s[i-k] in vowels: # remove old char
+                count -= 1
+            maxCount = max(maxCount, count)
+
+            # Early exit optimization: if we already found k vowels, can't get higher
+            if maxCount == k:
+                return k
 
         return maxCount
