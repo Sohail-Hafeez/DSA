17c6c58f3907a24eaa388c0dac66f71bb231c61f
Muhammad Sohail Hafeez <sohailhafeez6464@gmail.com>
2026-01-15 01:22:16 +0500

    Time: 55 ms (30.72%), Space: 29.1 MB (35.52%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,14 +1,35 @@
-class Solution(object):
+# The following code is correct but is brute force approach and its time complexity is O(n)
+
+# class Solution(object):
+#     def findDisappearedNumbers(self, nums):
+#         output = []
+#         j = 1
+#         for i in range(len(nums)):
+#             if j in nums:
+#                 j=j+1
+#                 continue
+#             else:
+#                 output.append(j)
+#                 j=j+1
+#         return output
+
+
+# optimal code 
+class Solution:
     def findDisappearedNumbers(self, nums):
+        n = len(nums)
+        
+        # Step 1: Mark numbers as visited
+        for num in nums:
+            index = abs(num) - 1   # map number to index
+            nums[index] = -abs(nums[index])
+        
+        # Step 2: Collect missing numbers
         output = []
-        j = 1
-        for i in range(len(nums)):
-            if j in nums:
-                j=j+1
-                continue
-            else:
-                output.append(j)
-                j=j+1
+        for i in range(n):
+            if nums[i] > 0:
+                output.append(i + 1)  # index i+1 is missing
+                
         return output
 
 
